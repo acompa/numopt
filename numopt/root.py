016d0bf6feac6f6606d0835c3f986d8eb6522d1d
Alex Companioni <achompas@gmail.com>
2014-09-15 03:05:08 -0400

    Root-finding methods.
    Newton's method, Wheeler's method (aka bent secant), regula falsi, and
    pure secant method.
    
    Tests, too.

--- numopt/root.py
+++ numopt/root.py
@@ -9,6 +9,33 @@ import logging
 logging.basicConfig(filename="root.log", level=logging.INFO)
 
 
+def _check_initial_value(f_initial, initial):
+    """
+    Given an initial value, check whether it is a root. If so, log and return.
+
+    :param f_initial:
+    :param initial:
+    :return:
+    """
+    if f_initial == 0:
+        logging.info("Initial value = {0:.16f} is a root.".format(initial))
+        return True
+    return False
+
+
+def _secant_update(current, prev, f_current, f_prev):
+    """
+    Return the secant update used in the secant method and regula falsi.
+
+    :param float current: current iterate
+    :param float prev: previous iterate
+    :param float f_current: function value @ current iterate
+    :param float f_prev: function value @ prev iterate
+    :return: next iterate value
+    """
+    return current - f_current * (current - prev) / (f_current - f_prev)
+
+
 def bisection(func, left, right, maxit, epsilon=0.0):
     """
     Use the bisection method to find roots for the given function f. Stopping conditions:
@@ -28,12 +55,13 @@ def bisection(func, left, right, maxit, epsilon=0.0):
     if func(left) * func(right) > 0:
         raise ValueError("Left and right interval endpoints do not seem to contain a zero!")
 
-    iter = 0
-    while iter < maxit:
+    logging.info("\nStarting bisection method!")
+    the_iter = 0
+    while the_iter < maxit:
         f_left = func(left)
         f_right = func(right)
         logging.info("Iteration {0}:  [{1:.16f}, {2:.16f}], f(left) = {3:.16f}, "
-                     "f(right) = {4:.16f}".format(iter, left, right, f_left, f_right))
+                     "f(right) = {4:.16f}".format(the_iter, left, right, f_left, f_right))
 
         # Evaluate midpoint.
         mid = (left + right) / 2
@@ -50,57 +78,178 @@ def bisection(func, left, right, maxit, epsilon=0.0):
             logging.info("Interval [{0:.16f}, {1:.16f}] is smaller than epsilon={3:.16f}. "
                     "Terminating...".format(left, right, epsilon))
             return None
-        iter += 1
+        the_iter += 1
     logging.info("Final interval: [{0:.16f}, {1:.16f}]".format(left, right))
     return None, (left, right)
 
 
-def newton(func, initial):
-    """
-    Use Newton's method to find func's roots.
-
-    :param function func: find this function's roots
-    :param float initial: initial guess for root
-    :return: root or None if none found
-    :rtype: float
-    """
-    pass
-
-
-def regula_falsi(func, x0, x1):
-    """
-    Use the regula falsi method to find a root for func.
-
-    :param function func: find this function's roots
-    :param x0: initial guess x0
-    :param x1: initial guess x1
-    :return: root or None if none found
-    :rtype: float
-    """
-    pass
-
-
-def secant(func, x0, x1):
+def secant(func, prev, current, maxit):
     """
     Use the secant method to find a root for func.
 
     :param function func: find this function's roots
-    :param x0: initial guess x0
-    :param x1: initial guess x1
-    :return: root or None if none found
-    :rtype: float
+    :param float prev: initial guess x0
+    :param float current: initial guess x1
+    :param int maxit: maximum #iterations
+    :return: root and interval; root is None if none found
+    :rtype: tuple(float, tuple(float, float))
     """
-    pass
+    if current == prev:
+        raise ValueError("x0 and x1 must differ.")
+
+    # Initial checks
+    f_current = func(current)
+    f_prev = func(prev)
+    if _check_initial_value(f_current, current): return current
+    if _check_initial_value(f_prev, prev): return prev
+    if f_current == f_prev:
+        # TODO (ac) what do I do here?
+        return None, (None, None)
+
+    the_iter = 0
+    logging.info("\nStarting secant iterations! Initial iterates: {0:.16f}, {1:.16f}".format(
+        prev, current))
+    while the_iter < maxit:
+        next_value = _secant_update(current, prev, f_current, f_prev)
+        f_next = func(next_value)
+        logging.info("Iteration {0}: x={1:.16f}, f(x)={2:.16f}".format(
+            the_iter, next_value, f_next))
+        if f_next == f_current or f_next == 0:
+            logging.info("Root = {0:.16f} found! Last two iterates: {1:.16f}, {2:.16f}".format(
+                next_value, prev, current))
+            return next_value, (prev, current)
+
+        # Update for next iteration
+        f_prev = f_current
+        prev = current
+        f_current = f_next
+        current = next_value
+
+        the_iter += 1
+    logging.info("No root found. Last two iterates: {0:.16f}, {1:.16f}".format(prev, current))
+    return None, (prev, current)
 
 
-def wheeler(func, x0, x1):
+def regula_falsi(func, star, current, maxit):
+    """
+    Use the regula falsi method to find a root for func.
+
+    :param function func: find this function's roots
+    :param float star: initial guess x0
+    :param float current: initial guess x1
+    :param int maxit: maximum #iterations
+    :return: root and interval; root is None if none found
+    :rtype: tuple(float, tuple(float, float))
+    """
+    if star == current:
+        raise ValueError("x0 must differ.")
+    f_star = func(star)
+    f_current = func(current)
+    if f_star * f_current >= 0:
+        raise ValueError("f(x0) and f(x1) must have different signs.")
+
+    the_iter = 0
+    logging.info("\nStarting regula falsi iterations! Initial iterates: {0:.16f}, {1:.16f}".format(
+        star, current))
+    while the_iter < maxit:
+        next_value = _secant_update(current, star, f_current, f_star)
+        f_next = func(next_value)
+        logging.info("Iteration {0}: x={1:.16f}, f(x)={2:.16f}".format(
+            the_iter, next_value, f_next))
+        if f_next == 0:
+            logging.info("Root = {0:.16f} found! x-star={1:.16f}, last iterate={2:.16f}".format(
+                next_value, star, current))
+            return next_value, (star, current)
+
+        # Update k. If signs differ, interval still contains root. Save current value as star.
+        if f_next * f_current < 0:
+            logging.info("Updating x-star. {0:.16f} --> {1:.16f}".format(star, current))
+            star = current
+            f_star = f_current
+
+        # Store the next iterate for the next iteration.
+        current = next_value
+        f_current = f_next
+        the_iter += 1
+    logging.info("No root found. x-star={0:.16f}, last iterate={1:.16f}".format(star, current))
+    return None, (star, current)
+
+
+def wheeler(func, star, current, maxit):
     """
     Use Wheeler's method to find a root for func.
 
     :param function func: find this function's roots
-    :param x0: initial guess x0
-    :param x1: initial guess x1
+    :param float star: initial guess x0
+    :param float current: initial guess x1
+    :param int maxit: maximum #iterations
     :return: root or None if none found
     :rtype: float
     """
-    pass
+    f_star = func(star)
+    f_current = func(current)
+    if _check_initial_value(f_star, star): return star
+    if _check_initial_value(f_current, current): return current
+    if f_current * f_star > 0:
+        # TODO (ac) what do i do here??
+        return None, (None, None)
+
+    mu = 1.0
+    the_iter = 0
+    logging.info("\nStarting Wheeler's iterations! Initial iterates: {0:.16f}, {1:.16f}".format(
+        star, current))
+    while the_iter < maxit:
+        next_value = _secant_update(current, star, f_current, mu * f_star)
+        f_next = func(next_value)
+        logging.info("Iteration {0}: x={1:.16f}, f(x)={2:.16f}".format(
+            the_iter, next_value, f_next))
+
+        # Reduce step size or reset it if signs change between values.
+        if f_next * f_current < 0:
+            mu = 1.0
+            star = current
+            f_star = f_current
+        elif f_next == 0:
+            logging.info("Root = {0:.16f} found! x-star={1:.16f}, last iterate={2:.16f}".format(
+                next_value, star, current))
+            return next_value, (star, current)
+        else:
+            mu /= 2.0
+
+        current = next_value
+        f_current = f_next
+        the_iter += 1
+    logging.info("No root found. x-star={0:.16f}, last iterate={1:.16f}".format(star, current))
+    return None, (star, current)
+
+
+def newton(func, derivative, value, maxit):
+    """
+    Use Newton's method to find func's roots.
+
+    :param function func: find this function's roots
+    :param function derivative: derivative of given function
+    :param float value: initial guess for root
+    :param int maxit: maximum #iterations
+    :return: root or None if none found
+    :rtype: float
+    """
+    f_value = func(value)
+    the_iter = 0
+    logging.info("\nStarting Newton's iterations! Initial value = {0:.16f}".format(value))
+    while the_iter < maxit:
+        next_value = value - f_value / derivative(value)
+        f_next = func(next_value)
+        logging.info("Iteration {0}: x={1:.16f}, f(x)={2:.16f}".format(
+            the_iter, next_value, f_next))
+
+        # Set up next iteration.
+        value = next_value
+        f_value = f_next
+        if f_value == 0:
+            logging.info("Root = {0:.16f} found!".format(next_value))
+            return value
+        the_iter += 1
+    logging.info("No root found. Last iterate={0:.16f}".format(value))
+    return None
+
--- numopt/tests/test_root.py
+++ numopt/tests/test_root.py
@@ -13,34 +13,85 @@ logging.basicConfig(filename="root.log", level=logging.INFO)
 
 class TestOneDimRootFinding(unittest.TestCase):
 
+    def setUp(self):
+        self.func = lambda x: x**3 - 8
+        self.derivative = lambda x: 3 * x**2
+        self.maxit = 12
+        self.desired_root = 2.0
+
     def test_root(self):
         pass
 
     def test_bisection_1b(self):
         """
-        This is the unit test for exercise 1.1b.
-
-        Test bisection method using parameters listed below.
+        Unit test for exercise 1.1b.
         """
         logging.info("\nANSWERS TO EXERCISE 1.1B")
-        func = lambda x: x**3 - 8
         left = 0.5
         right = 3.1
-        maxit = 12
 
         # The final interval should contain the desired root.
-        root, (left, right) = undertest.bisection(func, left, right, maxit)
-        desired_root = 2.0
-        self.assertTrue(left < desired_root and right > desired_root)
+        root, (left, right) = undertest.bisection(self.func, left, right, self.maxit)
+        self.assertTrue(_root_in_interval(self.desired_root, left, right))
 
-    def test_bisection_1d(self):
+    def test_bisection_1de(self):
         """
-        Unit test for exercise 1.1d.
+        Unit test for exercise 1.1d and 1.1e.
         """
         logging.info("\nANSWERS TO EXERCISE 1.1D")
         func = lambda x: x**7 - 7 * x**6 + 21 * x**5 - 35 * x**4 + 35 * x**3 - 21 * x**2 + 7 * x - 1
-        left = 0.95
-        right = 1.01
-        maxit = 12
+        starting_left = 0.95
+        starting_right = 1.01
 
-        root, (left, right) = undertest.bisection(func, left, right, maxit)
+        # Margaret chose this case as a funky example of bisection. The root should NOT be in
+        # the interval for this function.
+        root, (left, right) = undertest.bisection(func, starting_left, starting_right, self.maxit)
+        desired_root = 1.0
+        self.assertFalse(_root_in_interval(desired_root, left, right))
+
+        # Now let's try the factored form of func. Here the interval SHOULD contain the true root.
+        logging.info("\nRUNNING EXERCISE 1.1E")
+        factored_func = lambda x: (x - 1)**7
+        root, (left, right) = undertest.bisection(
+            factored_func, starting_left, starting_right, self.maxit)
+        self.assertTrue(_root_in_interval(desired_root, left, right))
+
+    def test_3(self):
+        """
+        Unit test for exercise 1.3c-g.
+        """
+        # Run Newton's method. Last starting point is wildly high. How will Newton perform??
+        starting_points = (0.1, 4.0, -0.2, -0.1)
+        assert len(starting_points) == 4
+
+        logging.info("\nRUNNING EXERCISE 1.3C")
+        newton_roots = [undertest.newton(self.func, self.derivative, x0, 50)
+                for x0 in starting_points]
+
+        # Run secant-based methods. Last interval has a high right endpoint. How will the algos do?
+        secant_intervals = [(0.9, 10.0), (-0.2, 3.0), (0.1, 6.0), (1.9, 20.0), (20.0, 1.9)]
+        assert len(secant_intervals) == 5
+        logging.info("\nRUNNING EXERCISE 1.3D")
+        secant_results = [undertest.secant(self.func, prev, current, self.maxit)
+                for (prev, current) in secant_intervals]
+        logging.info("\nRUNNING EXERCISE 1.3E")
+        regula_falsi_results = [undertest.regula_falsi(self.func, prev, current, 100)
+                for (prev, current) in secant_intervals]
+        logging.info("\nRUNNING EXERCISE 1.3F")
+        wheeler_results = [undertest.wheeler(self.func, prev, current, 20)
+                for (prev, current) in secant_intervals]
+
+    def test_figure34(self):
+        """
+        Unit test confirming iterations in figure 3.4.
+        """
+        star = 0.1
+        current = 1.37
+        func = lambda x: x**6 + 3 * x - 4
+
+        logging.info("\nCONFIRMING FIGURE 3.4")
+        rf_results = undertest.regula_falsi(func, star, current, 100)
+
+
+def _root_in_interval(root, left, right):
+    return left <= root <= right
